0cb4f17ce53f86fa08d60c353832e87eb6a5e172
zhaoxp <zhaoxp@youxuepai.com>
2019-07-19 16:32:23 +0800

    枚举

---
+++ class/enum.py
@@ -0,0 +1,14 @@
+from enum import Enum, unique
+
+# 枚举类
+@unique
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+print(Weekday(1))
