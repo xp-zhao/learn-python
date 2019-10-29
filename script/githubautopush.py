491ceb6dccaf41800d645bb82c8110c74839067e
xp-zhao <13688396271@163.com>
2019-10-29 10:17:55 +0800

    完善脚本

--- script/githubautopush.py
+++ script/githubautopush.py
@@ -6,7 +6,7 @@ import datetime
 #subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
 #returncode = subprocess.call(["git", "push"])
 returncode = 1
-while returncode == 1:
+while returncode != 0:
     returncode = subprocess.call(["git", "push"])
     print(returncode)
 print(returncode)
