b10975a3706c84280d1702daf8081896de07bcbd
xp-zhao <13688396271@163.com>
2019-10-29 10:11:00 +0800

    'github 自动提交脚本

--- script/githubautopush.py
+++ script/githubautopush.py
@@ -1,7 +1,12 @@
 import subprocess
 import datetime
 
-subprocess.call(["git", "status"])
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
-subprocess.call(["git", "push"])
+#subprocess.call(["git", "status"])
+#subprocess.call(["git", "add", "."])
+#subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
+#returncode = subprocess.call(["git", "push"])
+returncode = 1
+while returncode == 1:
+    returncode = subprocess.call(["git", "push"])
+    print(returncode)
+print(returncode)
