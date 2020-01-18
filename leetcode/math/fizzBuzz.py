4a87c93df863dfb1d9da5d456bf6eb9c427cf22e
xp-zhao <13688396271@163.com>
2020-01-18 11:22:30 +0800

    FizzBuzz

---
+++ leetcode/math/fizzBuzz.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> list:
+        str_list = []
+        a = "Fizz"
+        b = "Buzz"
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                str_list.append(a + b)
+            elif i % 3 == 0:
+                str_list.append(a)
+            elif i % 5 == 0:
+                str_list.append(b)
+            else:
+                str_list.append(i)
+        return str_list
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.fizzBuzz(15))
