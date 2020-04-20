f40832fc3fa33d12600dee68697ee67b2f1c775b
xp-zhao <13688396271@163.com>
2020-04-20 16:29:38 +0800

    时间库简单使用示例

---
+++ timetest/time_test.py
@@ -0,0 +1,16 @@
+import time
+
+# print(time.time())
+# print(time.localtime())
+# print(time.strftime('%Y-%m-%d %H:%M:%S'))
+# print(time.strftime('%Y%m%d'))
+
+import datetime
+
+print(datetime.datetime.now())
+new_time = datetime.timedelta(minutes=10)
+print(datetime.datetime.now() + new_time)
+
+one_day = datetime.datetime(2008, 5, 27)
+new_date = datetime.timedelta(days=10)
+print(one_day + new_date)
