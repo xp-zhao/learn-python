27240c9ae8ac9cb0048914a9dd20e6d7b6cb8fdf
xp-zhao <13688396271@163.com>
2020-04-30 11:48:59 +0800

    脚本打包

--- selenium_test.py
+++ selenium_test.py
@@ -8,10 +8,12 @@ from selenium.webdriver.chrome.options import Options
 
 from clipboard import clipboard as clip
 
-chrome_driver = 'D:\driver\chromedriver.exe'
+chrome_driver = 'chromedriver.exe'
 chrome_options = Options()
 # 设置chrome浏览器无界面模式
 chrome_options.add_argument('--headless')
+chrome_options.add_argument("--window-size=4000,1600")
+
 #  解决“chrome正受到自动测试软件的控制”信息栏显示，使用disable-infobars属性不生效问题
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
 driver = webdriver.Chrome(executable_path=chrome_driver, options=chrome_options)
@@ -20,7 +22,7 @@ driver.get('https://www.baidu.com')
 driver.maximize_window()
 driver.find_element_by_id('kw').send_keys('当前时间')
 driver.find_element_by_id('su').click()
-sleep(5)
+sleep(2)
 
 
 def sent_msg():
@@ -31,31 +33,31 @@ def sent_msg():
 
 
 def get_text():
-    date_format = "%Y-%m-%d %H:%M:%S"
-    end = datetime.now()
-    str_end = end.strftime(date_format)
-    start = end - timedelta(seconds=30)
-    str_start = start.strftime(date_format)
-    result = "时间段：[%s - %s]，系统正常" % (str_start, str_end)
+    end_time = datetime.now()
+    start = end_time - timedelta(minutes=30)
+    str_format = "%d月%d日-%02d:%02d-%02d:%02d" % (
+        end_time.month, end_time.day, end_time.hour, end_time.minute, start.hour, start.minute)
+    result = "客户端后台系统 %s 系统运行正常" % str_format
     return result
 
 
 def send_screenshot():
     # 截取当前窗口，并指定截图图片的保存位置
-    path = "D:\\baidu_img.png"
+    path = "screen_img.png"
     # 刷新
     driver.refresh()
     # 全屏
     driver.maximize_window()
-    sleep(5)
+    sleep(2)
     driver.get_screenshot_as_file(path)
+    print("保存截图")
     clip.setText(get_text())
     sent_msg()
     clip.setImage(path)
     sent_msg()
 
 
-schedule.every(30).seconds.do(send_screenshot)
+schedule.every(6).seconds.do(send_screenshot)
 
 while True:
     schedule.run_pending()
--- timetest/time_test.py
+++ timetest/time_test.py
@@ -5,12 +5,18 @@ import time
 # print(time.strftime('%Y-%m-%d %H:%M:%S'))
 # print(time.strftime('%Y%m%d'))
 
-import datetime
+from datetime import datetime, timedelta
 
-print(datetime.datetime.now())
-new_time = datetime.timedelta(minutes=10)
-print(datetime.datetime.now() + new_time)
+# print(datetime.datetime.now())
+# new_time = datetime.timedelta(minutes=10)
+# print(datetime.datetime.now() + new_time)
+# 
+# one_day = datetime.datetime(2008, 5, 27)
+# new_date = datetime.timedelta(days=10)
+# print(one_day + new_date)
 
-one_day = datetime.datetime(2008, 5, 27)
-new_date = datetime.timedelta(days=10)
-print(one_day + new_date)
+end_time = datetime.now()
+start = end_time - timedelta(minutes=30)
+str_format = "%d月%d日-%02d:%02d-%02d:%02d" % (
+    end_time.month, end_time.day, end_time.hour, end_time.minute, start.hour, start.minute)
+print(str_format)
