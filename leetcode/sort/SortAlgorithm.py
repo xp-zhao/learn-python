35217ba5a14d9c72d912eba51943ede0779b019b
xp-zhao <13688396271@163.com>
2020-10-13 17:02:44 +0800

    归并、快速排序

--- leetcode/sort/SortAlgorithm.py
+++ leetcode/sort/SortAlgorithm.py
@@ -34,9 +34,61 @@ def selection_sort(num_list: list):
             num_list[min_index] = temp
 
 
+def merge_sort(num_list: list) -> list:
+    if len(num_list) == 1:
+        return num_list
+    mid_index = int(len(num_list) / 2)
+    left = num_list[0:mid_index]
+    right = num_list[mid_index:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge_list(left, right)
+
+
+def merge_list(a: list, b: list) -> list:
+    merged = []
+    ai = bi = 0
+    while ai < len(a) and bi < len(b):
+        if a[ai] < b[bi]:
+            merged.append(a[ai])
+            ai += 1
+        else:
+            merged.append(b[bi])
+            bi += 1
+    while ai < len(a):
+        merged.append(a[ai])
+        ai += 1
+    while bi < len(b):
+        merged.append(b[bi])
+        bi += 1
+    return merged
+
+
+def quick_sort(num_list: list, left: int, right: int):
+    if left < right:
+        pivot = partition(num_list, left, right)
+        quick_sort(num_list, left, pivot - 1)
+        quick_sort(num_list, pivot + 1, right)
+
+
+def partition(num_list: list, left: int, right: int) -> int:
+    pivot = num_list[left]
+    while left < right:
+        while left < right and num_list[right] >= pivot:
+            right -= 1
+        num_list[left] = num_list[right]
+        while left < right and num_list[left] <= pivot:
+            left += 1
+        num_list[right] = num_list[left]
+    num_list[left] = pivot
+    return left
+
+
 if __name__ == '__main__':
     data_list = [2, 1, 5, 4, 3, 6]
     # bubble_sort(data_list)
     # insertion_sort(data_list)
-    selection_sort(data_list)
+    # selection_sort(data_list)
+    # print(merge_sort(data_list))
+    quick_sort(data_list, 0, len(data_list) - 1)
     print(data_list)
